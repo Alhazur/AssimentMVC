e5be86ce07f972f1c2e133e6ba5af9f5437e7f44
DC <DC@DESKTOP-557AL2C>
2019-03-12 23:31:13 +0100

    ok

--- AssimentMVC/Controllers/HomeController.cs
+++ AssimentMVC/Controllers/HomeController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AssimentMVC.Models;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -13,7 +14,7 @@ namespace AssimentMVC.Controllers
         // GET: /<controller>/
         public IActionResult Index()
         {
-            return View(); 
+            return View();
         }
 
         public IActionResult About()
@@ -34,6 +35,24 @@ namespace AssimentMVC.Controllers
         //Fever
         public IActionResult Fever()
         {
+            return View();
+        }
+
+        public IActionResult Fever(string fah, string cel)
+        {
+            Temperature temp = new Temperature();
+
+            double numFah = 0;
+            double numCel = 0;
+
+            fah = fah.Replace(".", ",");
+            cel = cel.Replace(".", ",");
+
+            double.TryParse(fah, out numFah);
+            double.TryParse(cel, out numCel);
+
+            ViewBag.CelsiusToFahrenheit = Temperature.CelsiusToFahrenheit(fah);
+
             return View();
         }
     }
---
+++ AssimentMVC/Models/Temperature.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AssimentMVC.Models
+{
+    public class Temperature
+    {
+        public static double CelsiusToFahrenheit(string celsiusTepm)
+        {
+            double celsius = double.Parse(celsiusTepm);
+            double fahrenheit = (celsius * 9 / 5) + 32;
+            return fahrenheit;            
+        }
+
+        public static double FahrenheitToCelsius(string fahrenheitTepm)
+        {
+            double fahrenheit = double.Parse(fahrenheitTepm);
+            double celsius = (fahrenheit - 32) * 5 / 9;
+            return celsius;
+        }
+    }
+}
